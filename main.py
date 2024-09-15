49b4af2808b95074d6bcc7ad355159d78fe86655
JunsuPark7 <158395384+JunsuPark7@users.noreply.github.com>
2024-09-15 16:56:05 +0900

    Add files via upload
    init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+
+from langchain_openai import ChatOpenAI  # 최신 ChatOpenAI 클래스
+import streamlit as st
+
+chat_model = ChatOpenAI()  # 여기 API 키 입력
+
+
+# print(result2.content)
+
+st.title("인공지능 시인")
+title = st.text_input("시의 주제를 제시해주세요.")
+
+
+if st.button('시 작성 요청하기'):
+    with st.spinner('시 작성중...'):
+        result = chat_model.invoke(title + "에 대한 시를 써줘")  # predict 대신 invoke 사용
+        st.write(result.content)
+
+
+
+
+
+
